38cc02d6ded222ee5382221af48eec2bca772b10
Barasa-Micah <barasamica@gmail.com>
2023-08-26 11:44:18 +0300

    Completed assignment

---
+++ lib/create.sql
@@ -0,0 +1,20 @@
+CREATE TABLE bears(
+       id INTEGER PRIMARY KEY,
+       name TEXT,
+       age INTEGER,
+       sex TEXT,
+       color TEXT,
+       Temperament TEXT,
+       alive, TRUE AND FALSE
+);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Mr.Chocolate',32, 'M', 'Chocolat
+e', 'choleric', 'True');
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Rowdy',24, 'M', 'White', 'phlegmatic', 'True');
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Tabitha',34, 'F', 'chocolate','m
+elancholic', 'True');
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Sergeant Brown', 46,'M', 'Dark', 'sanguine','True');
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ( 'Melissa', 29, 'F', 'White', 'phlegmatic', 'True');
+INSERT INTO bears (name,age ,sex,color,temperament,alive) VALUES ( 'Grinch',49, 'M','Dark', 'melencholic', 'True');
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Wendy', 28, 'F', 'chocolate', 's
+anguine', 'True');
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ( NULL, 57,'M', 'Dark', 'melancholic', 'True');
